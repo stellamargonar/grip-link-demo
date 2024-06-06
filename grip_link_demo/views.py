86d58b1b1a84e0c18a16cc976be8d24ca0d644cc
Daniele Isoni <isoni@spaziodati.eu>
2024-06-06 11:56:53 +0200

    make is expired work

--- grip_link_demo/views.py
+++ grip_link_demo/views.py
@@ -4,6 +4,7 @@ from urllib.parse import urljoin
 from django.http import HttpRequest, HttpResponse
 from django.urls import reverse
 from django.views import View
+from django.utils import timezone
 from django_grip import publish, set_hold_stream
 from gripcontrol import Channel, HttpStreamFormat
 
@@ -13,11 +14,22 @@ MODE = 1  # always 0 as prev id, does not work when a message is sent and receiv
 # MODE = 2  # set sequence id and prev id, works
 
 
+LAST_KNOWN_ID = None
+def update_last_known_id(id_):
+    global LAST_KNOWN_ID
+    LAST_KNOWN_ID = id_
+
+CHANNELS_START_TIME = {}
+def update_start_time(channel_str):
+    global CHANNELS_START_TIME
+    CHANNELS_START_TIME[channel_str] = timezone.now()
+
 class SSESpecializedChannelView(View):
     def get(self, request: HttpRequest, channel_uuid: str) -> HttpResponse:
         resp = HttpResponse(content_type="text/event-stream")
 
         prev_id = None if MODE == 0 else "0"
+        update_last_known_id(prev_id)
         channel = Channel(channel_uuid, prev_id=prev_id)
         set_hold_stream(
             request,
@@ -27,6 +39,7 @@ class SSESpecializedChannelView(View):
         )
         next_link = urljoin("http://localhost:8000", reverse("streaming-is-expired-channel"))
         request.grip.instruct.set_next_link(next_link, timeout=30)  # type:ignore[attr-defined]
+        update_start_time(channel_uuid)
         return resp
 
 
@@ -41,7 +54,7 @@ class SSEIsExpiredView(View):
         if channel_is_expired(channel_str):
             return resp
 
-        channel = Channel(channel_str)
+        channel = Channel(channel_str, prev_id=LAST_KNOWN_ID)
         set_hold_stream(
             request,
             channel,
@@ -60,11 +73,12 @@ class SSEGenerateMessages(View):
 
 
 def channel_is_expired(channel):
-    return False
+    channel_start_time = CHANNELS_START_TIME.get(channel)
+    return channel_start_time is not None and (timezone.now() - channel_start_time).total_seconds() > 60
 
 
 def publish_messages(channel_uuid):
-    text = "messaggio da base lunare alpha".split()
+    text = "Here is a streaming message".split()
 
     for i, tok in enumerate(text):
         message = "\n".join([f"data: {tok}", "", ""])
@@ -77,6 +91,7 @@ def publish_messages(channel_uuid):
         else:
             prev_id = str(i)
             id_ = str(i+1)
+            update_last_known_id(id_)
 
         print(f"> Send {tok}, id_ {id_}, prev_id {prev_id}")
         publish(channel_uuid, HttpStreamFormat(message + "\n"), prev_id=prev_id, id=id_)
